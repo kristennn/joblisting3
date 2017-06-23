5a1cd67545b6867edd57b13d6369636a1adb2166
13949836783@163.com <13949836783@163.com>
2017-06-23 16:59:32 +0800

    user can order jobs by wage and time

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,14 @@
 class JobsController < ApplicationController
 before_action :require_is_admin, only: [:show]
   def index
-    @jobs = Job.all
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.order('wage_lower_bound DESC')
+    when 'by_upper_bound'
+      Job.order("wage_upper_bound DESC")
+    else
+      Job.order("created_at DESC")
+    end
   end
 
   def show
