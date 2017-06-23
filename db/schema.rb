d85fa7c8f1cdb8ebdb851a4e9b9e0bb282f8bf6c
13949836783@163.com <13949836783@163.com>
2017-06-23 17:33:10 +0800

    user can apply job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623090304) do
+ActiveRecord::Schema.define(version: 20170623092121) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170623090304) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.text     "content"
+    t.string   "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "job_id"
   end
 
   create_table "users", force: :cascade do |t|
